31b68421753b0dc35b1306a04d26e37987d9056f
Maximillian von Briesen <mobyvb@gmail.com>
2013-03-27 23:05:35 -0400

    Euler 2 works now

--- ProjectEuler1/ProjectEuler/EntryPoint.cs
+++ ProjectEuler1/ProjectEuler/EntryPoint.cs
@@ -8,6 +8,15 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
+            EulerProject1();
+
+            OutputService.WriteLine("\n\n");
+
+            EulerProject2();
+        }
+
+        public static void EulerProject1()
+        {
             OutputService.WriteLine("Euler Project 1:");
             int range = -1;
             while (!ValidationService.IsPositive(range))
@@ -29,6 +38,39 @@ namespace ProjectEuler
                 {
                     totalSum += i;
                 }
+            }
+
+            OutputService.WriteLine("Final sum is " + totalSum + ".");
+        }
+
+        public static void EulerProject2()
+        {
+            OutputService.WriteLine("Euler Project 2:");
+            int limit = -1;
+            while (!ValidationService.IsPositive(limit))
+            {
+                limit = InputOutputService.GetIntFromPrompt("Input a limit (positive int):");
+                if (!ValidationService.IsPositive(limit))
+                {
+                    OutputService.WriteLine("Please enter a positive integer.");
+                }
+            }
+
+            int totalSum = 0;
+
+            int fibOld1 = 1;
+            int fibOld2 = 1;
+            int fibNew = 0;
+            while (fibNew <= limit)
+            {
+                fibNew = fibOld1 + fibOld2;
+                if (fibNew % 2 == 0)
+                {
+                    totalSum += fibNew;
+                }
+
+                fibOld2 = fibOld1;
+                fibOld1 = fibNew;
             }
 
             OutputService.WriteLine("Final sum is " + totalSum + ".");
